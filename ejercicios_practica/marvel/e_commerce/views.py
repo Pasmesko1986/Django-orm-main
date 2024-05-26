75ff20cb0c6f634657a6b31a94803d084ca67a7e
Pasmesko1986 <pasmesko1986@gmail.com>
2024-05-26 01:42:05 -0300

    .

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -49,23 +49,7 @@ def comic_list_order_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_list_order_api_view")
-        consulta = list(Comic.objects.order_by('marvel_id').values('title','stock_qty','price'))
+        consulta = list(Comic.objects.order_by('marvel_id').values('marvel_id','title','stock_qty','price'))
         return JsonResponse(consulta, safe=False)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
-
-#Lista de funicones realizadas
-
-#docker-compose up para levantar el repo
-#python manage.py shell para poder acceder a las funciones de Django ORM
-
-# ejemplo de una vista
-
-# from django.shortcuts import render
-# from .models import Comic
-
-# def comic_list(request):
-#     comics = Comic.objects.all()
-#     for comic in comics:
-#         print(f"Title: {comic.title}, Author: {comic.author}, Publication Date: {comic.publication_date}")
-#     return render(request, 'comics/comic_list.html', {'comics': comics})
